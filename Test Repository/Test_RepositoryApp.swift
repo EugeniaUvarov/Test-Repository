9a1bcc1b6a75777e12193cef0750b3c33fb5ebd0
Eugenia <eugenia.uvarov@icloud.com>
2024-08-21 16:45:51 -0700

    Initial Commit

---
+++ Test Repository/Test_RepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_RepositoryApp.swift
+//  Test Repository
+//
+//  Created by Eugenia Uvarov on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_RepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
